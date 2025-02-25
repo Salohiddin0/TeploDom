6a91e1a37987751176cfacdb49f7d9d1e89b5495
Your GitHub Username <Your GitHub Email>
2025-02-25 09:07:12 +0500

    Fixed delay issue when closing the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,61 +1,102 @@
-import React from "react";
-import logo from "../assets/navbar/logo.svg";
-import { Link } from "react-router-dom";
-import closeIcon from "../assets/modal/close.svg";
-import { useDispatch } from "react-redux";
-import { delModals } from "../features/modal/modalSlice";
+import React, { useState } from 'react'
+import logo from '../assets/navbar/logo.svg'
+import { Link, useNavigate } from 'react-router-dom'
+import closeIcon from '../assets/modal/close.svg'
+import { useDispatch } from 'react-redux'
+import { delModals } from '../features/modal/modalSlice'
+
 const Sidebar = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [isClosing, setIsClosing] = useState(false) // Animatsiya uchun state
+
+  const handleClose = path => {
+    setIsClosing(true)
+    setTimeout(() => {
+      dispatch(delModals())
+      navigate(path)
+    }, 500)
+  }
+
   return (
     <aside
-      onClick={() => dispatch(delModals())}
-      className="fixed top-0 left-0 z-[99] h-full w-full"
+      onClick={() => handleClose('/')}
+      className={`fixed top-0 left-0 z-[99] h-full w-full transition-opacity duration-500 
+      ${isClosing ? 'opacity-0' : 'opacity-100'} 
+      bg-black/50 backdrop-blur-sm`} // Orqa fon qorayadi + xiralashadi
     >
       <div
-        onClick={(e) => e.stopPropagation()}
-        className="h-full w-4/5 bg-white px-[15px] py-[20px] animate-sidebar"
+        onClick={e => e.stopPropagation()}
+        className={`h-full w-4/5 bg-white px-[15px] py-[20px] rounded-r-lg transition-transform duration-500 
+        ${isClosing ? '-translate-x-full' : 'translate-x-0'}`}
       >
-        <div className="flex justify-between items-center">
-          <Link to={"/"} className="flex items-center gap-2 cursor-pointer">
-            <img src={logo} alt="Navbar logo" className="w-14 sm:w-[70px]" />
-            <div className="text-center">
-              <h1 className="bg-linarOrange bg-clip-text text-transparent text-lg lg:!text-2xl font-semibold tracking-wider">
+        <div className='flex justify-between items-center'>
+          <Link
+            onClick={() => handleClose('/')}
+            className='flex items-center gap-2 cursor-pointer'
+          >
+            <img src={logo} alt='Navbar logo' className='w-14 sm:w-[70px]' />
+            <div className='text-center'>
+              <h1 className='bg-linarOrange bg-clip-text text-transparent text-lg lg:!text-2xl font-semibold tracking-wider'>
                 TEPLODOM
               </h1>
-              <p className="text-[10px] lg:text-sm leading-4">
+              <p className='text-[10px] lg:text-sm leading-4'>
                 Интернет магазин <br /> строй материалов
               </p>
             </div>
           </Link>
           <button
-            onClick={() => {
-              dispatch(delModals());
-            }}
-            className="p-3 rounded-full bg-[#F7F7F7] cursor-pointer"
+            onClick={() => handleClose('/')}
+            className='p-3 rounded-full bg-[#F7F7F7] cursor-pointer'
           >
-            <img src={closeIcon} alt="close icon" className="scale-110" />
+            <img src={closeIcon} alt='close icon' className='scale-110' />
           </button>
         </div>
-        <ul className="flex flex-col divide-y divide-black mt-5">
-          <li className="py-5 text-lg">
-            <Link to={"/sale"}>Товары по акции</Link>
+        <ul className='flex flex-col divide-y divide-black mt-5'>
+          <li className='py-5 text-lg'>
+            <button
+              onClick={() => handleClose('/sale')}
+              className='w-full text-left'
+            >
+              Товары по акции
+            </button>
           </li>
-          <li className="py-5 text-lg">
-            <Link to={"/new"}>Новинки</Link>
+          <li className='py-5 text-lg'>
+            <button
+              onClick={() => handleClose('/new')}
+              className='w-full text-left'
+            >
+              Новинки
+            </button>
           </li>
-          <li className="py-5 text-lg">
-            <Link to={"/toProviders"}>Поставщикам</Link>
+          <li className='py-5 text-lg'>
+            <button
+              onClick={() => handleClose('/toProviders')}
+              className='w-full text-left'
+            >
+              Поставщикам
+            </button>
           </li>
-          <li className="py-5 text-lg">
-            <Link to={"/contacts"}>Контакты</Link>
+          <li className='py-5 text-lg'>
+            <button
+              onClick={() => handleClose('/contacts')}
+              className='w-full text-left'
+            >
+              Контакты
+            </button>
           </li>
-          <li className="py-5 text-lg">
-            <Link to={"/returnProduct"}>Возврат товара</Link>
+          <li className='py-5 text-lg'>
+            <button
+              onClick={() => handleClose('/returnProduct')}
+              className='w-full text-left'
+            >
+              Возврат товара
+            </button>
           </li>
         </ul>
       </div>
     </aside>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
