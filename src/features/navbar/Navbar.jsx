62371eb80bcbd0886491708730048bb77664eaef
Your GitHub Username <Your GitHub Email>
2025-02-25 08:33:13 +0500

    The issues have been fixed

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -1,89 +1,118 @@
-import React, { useState } from "react";
-import logo from "../../assets/navbar/logo.svg";
-import filters from "../../assets/navbar/filters.svg";
-import sidebarIcon from "../../assets/navbar/sidebar.svg";
-import { Link, NavLink } from "react-router-dom";
-import BasketIcon from "./basket/BasketIcon";
-import FavouritesIcon from "./favourites/FavouritesIcon";
-import Form from "./Form";
-import Profile from "./Profile";
-import Filter from "../search/filter/Filter";
-import { useDispatch } from "react-redux";
-import { setModal } from "../modal/modalSlice";
+import React, { useState } from 'react'
+import logo from '../../assets/navbar/logo.svg'
+import filters from '../../assets/navbar/filters.svg'
+import sidebarIcon from '../../assets/navbar/sidebar.svg'
+import { Link, NavLink } from 'react-router-dom'
+import BasketIcon from './basket/BasketIcon'
+import FavouritesIcon from './favourites/FavouritesIcon'
+import Form from './Form'
+import Profile from './Profile'
+import Filter from '../search/filter/Filter'
+import { useDispatch } from 'react-redux'
+import { setModal } from '../modal/modalSlice'
 const Navbar = () => {
-  const dispatch = useDispatch();
-  const [filter, setFilter] = useState(false);
+  const dispatch = useDispatch()
+  const [filter, setFilter] = useState(false)
   const handleFilter = () => {
-    setFilter(true);
-  };
+    setFilter(true)
+  }
   return (
     <>
       {filter && (
         <div
           onClick={() => setFilter(false)}
-          className="px-[15px] fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen bg-black/30"
+          className='px-[15px] fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen bg-black/30'
         >
-          <Filter type="modal" closer={setFilter} />
+          <Filter type='modal' closer={setFilter} />
         </div>
       )}
-      <nav className="py-5 flex flex-col gap-10">
-        <div className="flex items-center justify-between flex-wrap md:flex-nowrap gap-y-[22px]">
+      <nav className='py-5 flex flex-col'>
+        <div className='flex items-center justify-between flex-wrap md:flex-nowrap gap-y-[22px]'>
           <Link
-            to={"/"}
-            className="flex items-center gap-2 cursor-pointer md:order-1"
+            to={'/'}
+            className='flex items-center gap-2 cursor-pointer md:order-1'
           >
-            <img src={logo} alt="Navbar logo" className="w-14 sm:w-[70px]" />
-            <div className="text-center">
-              <h1 className="bg-linarOrange bg-clip-text text-transparent xs:text-lg lg:!text-2xl font-semibold tracking-wider">
+            <img src={logo} alt='Navbar logo' className='w-14 sm:w-[70px]' />
+            <div className='text-center'>
+              <h1 className='bg-linarOrange bg-clip-text text-transparent xs:text-lg lg:!text-2xl font-semibold tracking-wider'>
                 TEPLODOM
               </h1>
-              <p className="text-[10px] lg:text-sm leading-4">
+              <p className='text-[10px] lg:text-sm leading-4'>
                 Интернет магазин <br /> строй материалов
               </p>
             </div>
           </Link>
 
-          <div className="flex gap-5 md:gap-3 lg:gap-5 items-center justify-center md:order-3">
+          <div className='flex gap-5 md:gap-3 lg:gap-5 items-center justify-center md:order-3'>
             <FavouritesIcon />
             <BasketIcon />
             <Profile />
           </div>
 
-          <div className="flex flex-1 items-center justify-evenly gap-2 sm:gap-3 md:order-2 md:flex-none">
+          <div className='flex flex-1 items-center justify-evenly gap-2 sm:gap-3 md:order-2 md:flex-none'>
             <img
-              onClick={() => dispatch(setModal("sidebar"))}
+              onClick={() => dispatch(setModal('sidebar'))}
               src={sidebarIcon}
-              className="nav-icon md:hidden"
+              className='nav-icon md:hidden'
             />
             <Form />
             <img
               src={filters}
-              className="nav-icon md:hidden"
+              className='nav-icon md:hidden'
               onClick={handleFilter}
             />
           </div>
         </div>
 
-        <div className="hidden md:flex justify-between">
-          <NavLink to={"/sale"} className="nav-sidebar">
+        <div className='hidden md:flex justify-between mt-[57px]'>
+          <NavLink
+            to={'/sale'}
+            className='md:text-[14px] font-semibold p-[11px] px-[45px] md:px-[25px] rounded-[10px] 
+             bg-[#EEEDED] cursor-pointer hover:bg-gradient-to-b hover:from-[#ffb12a] 
+             hover:to-[#ef8100] hover:text-white'
+          >
             Товары по акции
           </NavLink>
-          <NavLink to={"/new"} className="nav-sidebar">
+
+          <NavLink
+            to={'/new'}
+            className='md:text-[14px] font-semibold p-[11px] px-[45px] md:px-[25px] rounded-[10px] 
+             bg-[#EEEDED] cursor-pointer hover:bg-gradient-to-b hover:from-[#ffb12a] 
+             hover:to-[#ef8100] hover:text-white'
+          >
             Новинки
           </NavLink>
-          <NavLink to={"/toProviders"} className="nav-sidebar">
+
+          <NavLink
+            to={'/toProviders'}
+            className='md:text-[14px] font-semibold p-[11px] px-[45px] md:px-[25px] rounded-[10px] 
+             bg-[#EEEDED] cursor-pointer hover:bg-gradient-to-b hover:from-[#ffb12a] 
+             hover:to-[#ef8100] hover:text-white'
+          >
             Поставщикам
           </NavLink>
-          <NavLink to={"/contacts"} className="nav-sidebar">
+
+          <NavLink
+            to={'/contacts'}
+            className='md:text-[14px] font-semibold p-[11px] px-[45px] md:px-[25px] rounded-[10px] 
+             bg-[#EEEDED] cursor-pointer hover:bg-gradient-to-b hover:from-[#ffb12a] 
+             hover:to-[#ef8100] hover:text-white'
+          >
             Контакты
           </NavLink>
-          <NavLink to={"/returnProduct"} className="nav-sidebar">
+
+          <NavLink
+            to={'/returnProduct'}
+            className='md:text-[14px] font-semibold p-[11px] px-[45px] md:px-[25px] rounded-[10px] 
+             bg-[#EEEDED] cursor-pointer hover:bg-gradient-to-b hover:from-[#ffb12a] 
+             hover:to-[#ef8100] hover:text-white'
+          >
             Возврат товара
           </NavLink>
         </div>
       </nav>
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
